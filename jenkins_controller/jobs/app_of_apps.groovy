a0e0537625c71bfc7eba892717c9402527d2d0d3
Piotr Andrzejewski <piotr.andrzejewski@gmail.com>
2023-12-07 11:27:46 +0000

    Define DSL for Jenkins Controller jobs: frontend, backend, app_of_apps

---
+++ jenkins_controller/jobs/app_of_apps.groovy
@@ -0,0 +1,20 @@
+pipelineJob('app_of_apps') {
+    parameters {
+        stringParam('backendDockerTag', '', 'Backend docker image tag')
+        stringParam('frontendDockerTag', '', 'Frontend docker image tag')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        credentials("")
+                        url("https://github.com/pioand/App_of_apps.git")
+                    }
+                    branches('main')
+                    scriptPath('Jenkinsfile')
+                }
+            }
+        }
+    }
+}
